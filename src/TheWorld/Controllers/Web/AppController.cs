1455c59bddc821f331b0e5f7156c0a717d9f4cd0
Edde <edde@outlook.com>
2017-01-22 15:36:45 -0800

    Initial commit

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Services;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController: Controller
+    {
+        private IMailService _mailservice;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService service, IConfigurationRoot config)
+        {
+            _mailservice = service;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel m)
+        {
+            _mailservice.SendMail(m.Email, _config["MailSettings:ToAddress"], "Subject Test", m.Message);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TheWorld/Services/DebugMailService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Services
+{
+    public class DebugMailService:IMailService
+    {
+        public void SendMail(string @from, string to, string subject, string body)
+        {
+            Debug.WriteLine($"From: {from}\n" +
+                            $"To: {to}\n" +
+                            $"Subject: {subject}\n" +
+                            $"Body: {body}");
+        }
+    }
+}
